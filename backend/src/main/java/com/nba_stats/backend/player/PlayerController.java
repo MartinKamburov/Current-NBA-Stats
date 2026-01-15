5a79592a85c3f756bd11a90045821f61baed133b
MartinKamburov <martin.kamburov@yahoo.ca>
2026-01-14 22:24:40 -0500

    Added a simple api route to get the leagues from the nba api

--- backend/src/main/java/com/nba_stats/backend/player/PlayerController.java
+++ backend/src/main/java/com/nba_stats/backend/player/PlayerController.java
@@ -1,18 +1,32 @@
 package com.nba_stats.backend.player;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+import tools.jackson.databind.JsonNode;
 
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping(path = "/api/v1")
 public class PlayerController {
 
-    @GetMapping //This handles http get requests
-    public void getPlayers(){
+    @Value("${nba_stats_api_key}")
+    private String NBA_API_KEY;
 
+    @GetMapping("/getleagues") //This handles http get requests
+    public String getLeagues(){
+        RestClient restClient = RestClient.builder()
+                .baseUrl("https://v1.basketball.api-sports.io")
+                .build();
+
+        return restClient.get()
+                .uri("/leagues")
+                .header("x-apisports-key", NBA_API_KEY) // Note: This API uses custom headers, not Basic Auth
+                .retrieve()
+                .body(String.class);
     }
 
 }
